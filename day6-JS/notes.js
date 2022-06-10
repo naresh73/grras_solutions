5b1570f470d95460bd39275aee5e014a8f874d71
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-06-10 07:54:19 +0530

    Assignment 1

---
+++ day6-JS/notes.js
@@ -0,0 +1,13 @@
+// Global scope :- varialbe declared globaly.
+//:-global scope variable can be changed and used anywhere in a file.
+
+// Functional scope  :- variable declared in a function black.
+//:-functional scope variable can be changed and used inside a function's bloack.
+
+// var :- 1. var is a global scope variable
+// 2. value can be change
+
+// let :- 1. can't declare let again
+// 3. value can be change
+
+// const:- 1. in const once assigned value cannot change.
