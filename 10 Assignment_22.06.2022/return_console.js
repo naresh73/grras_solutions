7c65b83338b560b48f0a61a1c5accdf84f8448cf
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-06-23 07:23:53 +0530

    return() / console.log()

---
+++ 10 Assignment_22.06.2022/return_console.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    // return() : return() is used for returning values from a function.
+    return (a + b);
+}
+console.log("sum of A + B = ",sum(134,245));
+
+function sab(a,b){
+    // console.log() : used for printing the statements.
+    console.log("sub of A - B = ",(a-b));
+}
+sab(345,199)
